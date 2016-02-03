bb357c654224924d9211e1d655e84dd4959df4f3
Gereon Kaiping <anaphory@yahoo.de>
2016-02-03 04:31:23 +0000

    There was a reason for the implementation of SelectorSet

--- src/parameterclone/selector/SelectorSet.java
+++ src/parameterclone/selector/SelectorSet.java
@@ -6,7 +6,7 @@ import beast.core.Input;
 import beast.core.Input.Validate;
 import beast.core.parameter.IntegerParameter;
 
-@Description("A calculation node that propagates the used values from a vector of parameters")
+@Description("A calculation node that propagates the used values from a vector of parameters. As opposed to Selector, this class aggregates values into some kind of multiset, useful for analyses where only different values matter.")
 @Citation("Huelsenbeck, J.P., Larget, B., Alfaro, M.E., 2004. "
 		+ "Bayesian Phylogenetic Model Selection Using Reversible Jump Markov Chain Monte Carlo. "
 		+ "Mol Biol Evol 21, 1123-1133. doi:10.1093/molbev/msh123")
@@ -15,17 +15,17 @@ public class SelectorSet extends Selector {
 			"sizes", "stores how many indices are pointing to each parameter",
 			Validate.REQUIRED);
 	
-//	@Override
-//	public int getDimension() {
-//		int dim = 0;
-//		Integer[] sizes = sizesInput.get().getValues();
-//		for (int i = 0; i<maxIndex; ++i){
-//			if (sizes[i] > 0) {
-//				++dim;
-//			}
-//		}
-//		return dim;
-//	}
+	@Override
+	public int getDimension() {
+		int dim = 0;
+		Integer[] sizes = sizesInput.get().getValues();
+		for (int i = 0; i<maxIndex; ++i){
+			if (sizes[i] > 0) {
+				++dim;
+			}
+		}
+		return dim;
+	}
 	
 	@Override
 	public double getArrayValue() {
